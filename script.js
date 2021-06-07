00bd31a9c1d0aa2e9c79f2fb08458b94326d903f
Skyler Novak <skylernovak@yahoo.com>
2021-06-06 18:26:32 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const character = {
+    race: ['Human', 'Elf', 'Dwarf', 'Halfling', 'Half-Elf', 'Half-Orc', 'Gnome', 'Tiefling', 'Dragonborn'],
+    class: ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rouge', 'Sorcerer', 'Warlock', 'Wizard'],
+    action: ['Melee Attack', 'Ranged Attack', 'Grapples', 'Dashes', 'Disengages', 'Uses an item', 'rests']
+};
+
+console.log(character);
